dc87d9834681b343b5b136471b9c621424b7c3f7
Anton Osadchenko <121436438+Osdantes@users.noreply.github.com>
2024-04-17 18:12:35 +0300

    Update ClientStats.java

--- stats-server/stats-client/src/main/java/ru/practicum/client/ClientStats.java
+++ stats-server/stats-client/src/main/java/ru/practicum/client/ClientStats.java
@@ -22,40 +22,43 @@ public class ClientStats {
         this.rest = rest;
     }
 
-    protected <T> ResponseEntity<T> get(String path, @Nullable Map<String, Object> parameters, ParameterizedTypeReference<T> type) {
-        return get(path, null, parameters, type);
+    protected ResponseEntity<Object> get(String path) {
+        return get(path, null, null);
     }
 
-    protected <T> ResponseEntity<T> get(String path, @Nullable Long userId, @Nullable Map<String, Object> parameters, ParameterizedTypeReference<T> type) {
-        return makeAndSendRequest(HttpMethod.GET, path, userId, parameters, null, type);
+    protected ResponseEntity<Object> get(String path, long userId) {
+        return get(path, userId, null);
     }
 
-    protected <T> ResponseEntity<T> post(String path, Object body, ParameterizedTypeReference<T> type) {
-        return post(path, null, null, body, type);
+    protected ResponseEntity<Object> get(String path, Long userId, @Nullable Map<String, Object> parameters) {
+        return makeAndSendRequest(HttpMethod.GET, path, userId, parameters, null);
     }
 
-    protected <T> ResponseEntity<T> post(String path, long userId, Object body, ParameterizedTypeReference<T> type) {
-        return post(path, userId, null, body, type);
+    protected <T> ResponseEntity<Object> post(String path, T body) {
+        return post(path, null, null, body);
     }
 
-    protected ResponseEntity<Object> post(String path, Object body) {
-        return post(path, body);
+    protected <T> ResponseEntity<Object> post(String path, long userId, T body) {
+        return post(path, userId, null, body);
     }
 
-    protected <T> ResponseEntity<T> post(String path, Long userId, @Nullable Map<String, Object> parameters, Object body, ParameterizedTypeReference<T> type) {
-        return makeAndSendRequest(HttpMethod.POST, path, userId, parameters, body, type);
+    protected <T> ResponseEntity<Object> post(String path, Long userId, @Nullable Map<String, Object> parameters, T body) {
+        return makeAndSendRequest(HttpMethod.POST, path, userId, parameters, body);
     }
 
-    private <T> ResponseEntity<T> makeAndSendRequest(HttpMethod method, String path, Long userId, @Nullable Map<String, Object> parameters, @Nullable Object body, ParameterizedTypeReference<T> type) {
-        HttpEntity<Object> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
+   private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, Long userId, @Nullable Map<String, Object> parameters, @Nullable T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
 
-        ResponseEntity<T> shareitServerResponse;
-        if (parameters != null) {
-            shareitServerResponse = rest.exchange(path, method, requestEntity, type, parameters);
-        } else {
-            shareitServerResponse = rest.exchange(path, method, requestEntity, type);
+        ResponseEntity<Object> shareitServerResponse;
+        try {
+            if (parameters != null) {
+                shareitServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+            } else {
+                shareitServerResponse = rest.exchange(path, method, requestEntity, Object.class);
+            }
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
-
         return prepareGatewayResponse(shareitServerResponse);
     }
 
@@ -69,29 +72,17 @@ public class ClientStats {
         return headers;
     }
 
-    private static <T> ResponseEntity<T> prepareGatewayResponse(ResponseEntity<T> response) {
+    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
         if (response.getStatusCode().is2xxSuccessful()) {
             return response;
         }
 
         ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
+
         if (response.hasBody()) {
             return responseBuilder.body(response.getBody());
         }
 
         return responseBuilder.build();
-    }
-
-    public ResponseEntity<List<EndpointHitStatDto>> getStatistics(String start, String end, String[] uris, Boolean unique) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("start", start);
-        params.put("end", end);
-        params.put("uris", uris);
-        params.put("unique", unique);
-        return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", params, new ParameterizedTypeReference<>() {});
-    }
-
-    public void addHit(String app, String uri, String ip, LocalDateTime timestamp) {
-        post("/hit", new EndpointHitDto(app, uri, ip, df.format(timestamp)), new ParameterizedTypeReference<Void>() {});
     }
 }
